124942f6ac7d53b479058a73292d22e42ba6bcc8
Ran-n <ran-n@tutanota.com>
2021-07-05 17:41:59 +0200

    creado ficheiro e cargar

---
+++ ficheiro.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python3
+# --------------------------------------------------
+#+ Autor:	Ran#
+#+ Creado:	05/07/2021 17:36:35
+#+ Editado:	05/07/2021 17:41:43
+# --------------------------------------------------
+import os
+# --------------------------------------------------
+def cargar(fich_nome, encoding='utf-8-sig'):
+    # se o ficheiro a cargar non existe sacar erro
+    if not os.path.isfile(fich_nome):
+        raise Exception('O ficheiro {} non existe'.format(nome))
+
+    # faise neste bloque en lugar de directamente para poder pechar a conexión
+    # de non pechala pódese corromper
+    try:
+        conn = open(fich_nome, 'r', encoding=encoding)
+        fich_contido = conn.read()
+    except:
+        # saca o erro producido
+        raise
+    finally:
+        conn.close()
+        return fich_contido
+# --------------------------------------------------
