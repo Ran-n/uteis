4e5ff4e9c13a7fd9a4a40549c3543060c1597e23
Ran-n <ran-n@tutanota.com>
2021-07-13 15:06:16 +0200

    cambios, melloras e gardarFich

--- uteis/ficheiro.py
+++ uteis/ficheiro.py
@@ -3,7 +3,7 @@
 # --------------------------------------------
 #+ Autor:	Ran#
 #+ Creado:	05/07/2021 17:36:35
-#+ Editado:	05/07/2021 21:49:54
+#+ Editado:	13/07/2021 15:05:38
 # --------------------------------------------
 
 import os
@@ -12,20 +12,23 @@ import json
 # --------------------------------------------
 
 # carga os contidos dun ficheiro
-def cargarFich(fich_nome, encoding='utf-8-sig'):
+def cargarFich(fich_nome, en_linhas=True, encoding='utf-8-sig'):
     """
     Dado un catex, ou lista con catexs, de entrada;
     devolve esta mesma estrutura pero cos catexs riscados.
 
     @entrada:
         fich_nome   -   Requerido   -   Catex/Lista
-        └ Nome do ficheiro a cargar.
+        └ Nome do ficheiro a cargar, con extensión.
+        en_linhas   -   Opcional    -   Booleano
+        └ True: carga nun array cada linha como catex.
+        └ False: carga nun só catex con todo o ficheiro.
         encoding    -   Opcional    -   Catex
         └ Tipo de codificación a usar.
 
-    @saida:
+    @saída:
         Catex   -   Sempre
-        └ A mesma entrada, pero riscada
+        └ O contido do ficheiro.
     """
     # se o ficheiro a cargar non existe sacar erro
     if not os.path.isfile(fich_nome):
@@ -35,13 +38,58 @@ def cargarFich(fich_nome, encoding='utf-8-sig'):
     # de non pechala pódese corromper
     try:
         conn = open(fich_nome, 'r', encoding=encoding)
-        fich_contido = conn.read()
+        if en_linhas:
+            fich_contido = conn.readlines()
+        else:
+            fich_contido = conn.read()
+
     except:
         # saca o erro producido
         raise
-    finally:
-        conn.close()
+
+    # se non ocorre ningunha excepción
+    else:
         return fich_contido
+
+    finally:
+        if 'conn' in globals():
+            conn.close()
+
+def gardarFich(fich_nome, contido, encoding='utf-8-sig'):
+    """
+    Dado un nome de ficheiro (con ou sen path/caminho) gardao en memoria.
+    Se non se da camiño na propia carpeta de execución e senón non camiño dado.
+
+    @entrada:
+        fich_nome   -   Requerido   -   Catex
+        └ Nome do ficheiro a gardar, con extensión. Con ou sen o camiño incluído.
+        contido     -   Requerido   -   Catex
+        └ Catex a gardar dentro do ficheiro.
+        encoding    -   Opcional    -   Catex
+        └ Tipo de codificación a usar.
+
+    @saída:
+        Booleano    -   Cando todo foi correcto.
+        └ Verdadeiro:   Indica o correcto funcionamento.
+    """
+    try:
+        os.makedirs('/'.join(fich_nome.split('/')[:-1])), exist_ok=True)
+
+        conn = open(fich_nome.split('/')[-1]
+        for ele in contido:
+            conn.writelines(ele+'\n')
+
+    except:
+        # saca o erro producido
+        raise
+
+    else:
+        return True
+
+    finally:
+        # se existe a variable
+        if 'conn' in globals():
+            conn.close()
 
 # garda nun ficheiro os contidos dun diccionario de python no formato json
 def jgardarFich(fich_nome, contido, indent=1, sort_keys=False, ensure_ascii=False):
@@ -61,10 +109,9 @@ def jgardarFich(fich_nome, contido, indent=1, sort_keys=False, ensure_ascii=Fals
         └ De estar a True, asegurase que o resultado sexan caracters
             ascii válidos e que se devolva un obxecto de tipo unicode.
 
-
     @saida:
-        True    -   Se é exitoso.
-        False   -   De non ser exitoso.
+        Booleano    -   Canto todo foi correcto.
+        └ Verdadeiro:   Indica o correcto funcionamento.
     """
 
     # faise neste bloque en lugar de directamente para poder pechar a conexión
@@ -72,13 +119,17 @@ def jgardarFich(fich_nome, contido, indent=1, sort_keys=False, ensure_ascii=Fals
     try:
         conn = open(ficheiro, 'w')
         conn.write(json.dumps(contido, indent=indent, sort_keys=sort_keys, ensure_ascii=ensure_ascii))
+
     except:
         # saca o erro producido
         raise
-        return False
-    finally:
-        conn.close()
+
+    else:
         return True
+
+    finally:
+        if 'conn' in globals():
+            conn.close()
 
 # --------------------------------------------
 
