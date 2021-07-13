d52d108409615ccc1e751319791227d10abe424a
Ran-n <ran-n@tutanota.com>
2021-07-13 13:34:02 +0200

    empaquetado

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='uteis',
+    version='0.1.0',
+    description='Paquetes con funcións uteis para programar en python',
+    url='https://wwww.github.com/Ran-n/uteis',
+    author='Ran#',
+    author_email='ran-n@tutanota.com',
+    license='GPL 3.0',
+    packages=['uteis'],
+    install_requires=[],
+
+    classifiers=[
+        'Estado de desenvolvemento :: 1 - Inicial',
+        'Audiencia Esperada :: Eu mesmo',
+        'Licencia :: GNU General Public License 3.0 :: GPL 3.0',
+        'Linguaxe de programación :: Python 2.7.18',
+    ],
+)
